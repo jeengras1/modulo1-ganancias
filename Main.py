ca40b7f973f9e59639e7b2512ec11408dbc27b8f
jeengras1 <jeissonmm@gmail.com>
2025-06-21 16:39:58 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '✅ Módulo 1 – Ganancias funcionando desde Cloud Run'
+@app.route("/")
+def home():
+    return "✅ Módulo 1 – Ganancias ejecutado desde Cloud Run correctamente."
 
-if __name__ == '__main__':
-    # 👇 Esto es crítico: escuchar en el puerto proporcionado por Cloud Run
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Cloud Run exige este puerto
+    app.run(host="0.0.0.0", port=port)        # Cloud Run exige este host
