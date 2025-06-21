ae18adf910eade693571333aee1559d18471eaa0
jeengras1 <jeissonmm@gmail.com>
2025-06-21 16:47:06 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,9 +4,9 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "✅ Módulo 1 – Ganancias ejecutado desde Cloud Run correctamente."
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Cloud Run exige este puerto
-    app.run(host="0.0.0.0", port=port)        # Cloud Run exige este host
+    port = int(os.environ.get("PORT", 8080))  # Puerto que Cloud Run requiere
+    app.run(host="0.0.0.0", port=port)        # Cloud Run exige host 0.0.0.0
